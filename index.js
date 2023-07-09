1d23c45889ca15e8c564988880cd9c4ca1a1f00b
Kelvin okumba <okumbakelvin@gmail.com>
2023-07-09 15:41:14 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+
+const currentUser="Grace Hopper";
+const welcomeMessage="Welcome to Flatbook, Grace Hopper ${currentUser}!";
+const exitedWelcomeMessage ="WELCOME TO FLATBOOK, Grace Hopper!";
+const shortGreeting="welcome, ${currentUser.slice(0, 2)!";
